5a239ed0f1c8530d41d648af6d8f758404273777
Priyadharshinik4 <priyadharshinik1720@gmail.com>
2024-09-02 17:57:58 +0530

    first commit

---
+++ src/components/Youtube.js
@@ -0,0 +1,115 @@
+import React from "react";
+import { IoMenu } from "react-icons/io5";
+import { FaYoutube } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import { MdHome } from "react-icons/md";
+import { IoMusicalNotesOutline } from "react-icons/io5";
+import { IoLink } from "react-icons/io5";
+import { MdInbox } from "react-icons/md";
+import { IoDiamond } from "react-icons/io5";
+import { FaCircleUser } from "react-icons/fa6";
+import { LiaHospitalSymbolSolid } from "react-icons/lia";
+import { BiShapeTriangle } from "react-icons/bi";
+
+function Youtube(){
+    return(
+        <div>
+            <div className="header">
+                <h5><IoMenu /></h5>
+                <h2><span><FaYoutube /></span>Youtube</h2>
+                <div className="search">
+                    <div className="search-bar">
+                        <input type="text" placeholder="Search..." />
+                        <FaSearch className="search-icon" />
+                    </div>
+                </div>
+            </div>
+            <div className="sidemenu">
+                <h4><MdHome /></h4>
+                <h4><IoMusicalNotesOutline /></h4>
+                <h4><IoLink /></h4>
+                <h4><MdInbox /></h4>
+                <h4><IoDiamond /></h4>
+                <h4><BiShapeTriangle /></h4>
+                <h4><LiaHospitalSymbolSolid /></h4>
+                <h4><FaCircleUser /></h4>
+            </div>
+            <div className="videos">
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+                <div className="one">
+                    <iframe width="230" height="200" src="https://www.youtube.com/embed/Iy8lSoRzElo?si=SY_nIbTdC1Ukb7GS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <h3>Men's 4x100m Final | Paris Champions</h3>
+                    <h4>Olympics</h4>
+                    <h5>2 weeks ago</h5>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Youtube;
